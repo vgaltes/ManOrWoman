54bfb9aac26f80f218299a9ef7bf5e963768cae1
Vicenc Garcia-Altes <vincentredrum@gmail.com>
2017-03-30 13:41:45 +0100

    Return proper json

--- ManOrWoman/run.fsx
+++ ManOrWoman/run.fsx
@@ -22,6 +22,7 @@ type NameData = CsvProvider<"Order,Name,Frequency,AverageAge", HasHeaders = true
 
 type NameStatistic = {Frequency: int}
 
+[<CLIMutable>]
 type Result = {
     Gender: string
     Frequency: int
@@ -84,7 +85,7 @@ let Run(req: HttpRequestMessage, log: TraceWriter) =
             | Some x ->
                 let statistics = getNameStatistics x.Value log
                 match statistics with
-                | Some y -> req.CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(y));
+                | Some y -> req.CreateResponse(HttpStatusCode.OK, y);
                 | None -> req.CreateResponse(HttpStatusCode.BadRequest, "We haven't found the name");
             | None ->
                 req.CreateResponse(HttpStatusCode.BadRequest, "Specify a Name value");
